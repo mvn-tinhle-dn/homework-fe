cb8d9da765ecda11244d1517673f64fde27eda22
Tinh Le Viet <tinh.le@monstar-lab.com>
2022-04-12 18:14:45 +0700

    Update: Fix fotmat v1

--- hw-react1/src/index.js
+++ hw-react1/src/index.js
@@ -8,15 +8,15 @@ import Comment from './components/Comment';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const author = {
-	name: 'Tinh Le',
-	text: "Today it's really beautiful!",
-	date: '12-04-2022',
-	avatarUrl: 'https://ca.slack-edge.com/T7Z35JWLQ-U033ZREE8M6-5d6bea07df8f-512'
+  name: 'Tinh Le',
+  text: "Today it's really beautiful!",
+  date: '12-04-2022',
+  avatarUrl: 'https://ca.slack-edge.com/T7Z35JWLQ-U033ZREE8M6-5d6bea07df8f-512'
 };
 root.render(
   <React.StrictMode>
     <App />
-		<Comment author = {author}/>
+    <Comment author={author} />
   </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
