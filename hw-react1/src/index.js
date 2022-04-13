4c17d9a6f6abf2185fba7d829f8636f07c65989f
Tinh Le Viet <tinh.le@monstar-lab.com>
2022-04-13 14:15:33 +0700

    Update: Reset app

--- hw-react1/src/index.js
+++ hw-react1/src/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/global.scss';
-import './styles/comment.scss';
 import App from './views/App';
 import reportWebVitals from './reportWebVitals';
-import Comment from './views/Comment';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const author = {
-  name: 'Tinh Le',
-  text: "Today it's really beautiful!",
-  date: '12-04-2022',
-  avatarUrl: 'https://ca.slack-edge.com/T7Z35JWLQ-U033ZREE8M6-5d6bea07df8f-512'
-};
 root.render(
   <React.StrictMode>
     <App />
-    <Comment author={author} />
   </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
