8460b42087477d82322fb3498235588a76c848cc
Tinh Le Viet <tinh.le@monstar-lab.com>
2022-04-22 17:23:09 +0700

    Update: formatv1

--- hw-react1/src/components/TableUserComponent.js
+++ hw-react1/src/components/TableUserComponent.js
@@ -1,26 +1,26 @@
 import React from "react";
 import TableItemComponent from "./TableItemComponent";
 export default class TableUserComponent extends React.Component {
-	render() {
-		const { arrUser, handleDeleteAUser } = this.props;
-		return (
-			<table>
-				<thead>
-					<tr>
-						<th>No.</th>
-						<th>ID User</th>
-						<th>Email</th>
-						<th>Country</th>
-						<th>Gender</th>
-						<th>Info</th>
-						<th>Actions</th>
-					</tr>
-				</thead>
-				<TableItemComponent
-					arrUser={arrUser}
-					handleDeleteAUser={(item) => handleDeleteAUser(item)}
-				/>
-			</table>
-		);
-	}
+  render() {
+    const { arrUser, handleDeleteAUser } = this.props;
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>ID User</th>
+            <th>Email</th>
+            <th>Country</th>
+            <th>Gender</th>
+            <th>Info</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <TableItemComponent
+          arrUser={arrUser}
+          handleDeleteAUser={(item) => handleDeleteAUser(item)}
+        />
+      </table>
+    );
+  }
 }
